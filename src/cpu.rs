401c5b2861bf3a81298b1e08f6893ef3a9a9ff86
Ilya Rubnich <ilya@rubni.ch>
2024-09-12 21:52:07 -0400

    more

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,6 +1,11 @@
 use rs6502::Variant;
 use rs6502::memory::Bus;
 use rs6502::registers::Registers;
+use rs6502::instruction::{AddressingMode, DecodedInstr, Instruction, OpInput};
+
+fn address_from_bytes(lo: u8, hi: u8) -> u16 {
+    u16::from(lo) + (u16::from(hi) << 8usize)
+}
 
 pub struct CPU<M, V>
 where
@@ -18,6 +23,92 @@ impl<M: Bus, V: Variant> CPU<M, V> {
             registers: Registers::new(),
             memory,
             variant: core::marker::PhantomData::<V>,
+        }
+    }
+
+    pub fn reset(&mut self) {
+        // noop
+    }
+
+    pub fn fetch_next_and_decode(&mut self) -> Option<DecodedInstr> {
+        fn read_address<M: Bus>(mem: &mut M, addr: u16) -> [u8; 2] {
+            let lo = mem.get_byte(addr);
+            let hi = mem.get_byte(addr.wrapping_add(1));
+            [lo, hi]
+        }
+        
+        let x: u8 = self.memory.get_byte(self.registers.pc);
+
+        match V::decode(x) {
+            Some((instr, am)) => {
+                let extra_bytes = am.extra_bytes();
+                let num_bytes = extra_bytes + 1;
+
+                let data_start = self.registers.pc.wrapping_add(1);
+
+                let slice = if extra_bytes == 0 {
+                    [0, 0]
+                } else if extra_bytes == 1 {
+                    [self.memory.get_byte(data_start), 0]
+                } else if extra_bytes == 2 {
+                    [
+                        self.memory.get_byte(data_start),
+                        self.memory.get_byte(data_start.wrapping_add(1)),
+                    ]
+                } else {
+                    panic!()
+                };
+
+                let x = self.registers.x;
+                let y = self.registers.y;
+
+                let memory = &mut self.memory;
+
+                let am_out = match am {
+                    AddressingMode::IMP => OpInput::UseImplied,
+                    AddressingMode::IMM => OpInput::UseImmediate(slice[0]),
+                    AddressingMode::ZP0 => OpInput::UseAddress(u16::from(slice[0])),
+                    AddressingMode::ZPX => OpInput::UseAddress(u16::from(slice[0].wrapping_add(x))),
+                    AddressingMode::ZPY => OpInput::UseAddress(u16::from(slice[0].wrapping_add(y))),
+                    AddressingMode::REL => {
+                        let offset = slice[0];
+                        let sign_extend = if offset & 0x80 == 0x80 { 0xFFu8 } else { 0x0 };
+                        let rel = u16::from_le_bytes([offset, sign_extend]);
+                        OpInput::UseRelative(rel)
+                    },
+                    AddressingMode::ABS => OpInput::UseAddress(address_from_bytes(slice[0], slice[1])),
+                    AddressingMode::ABX => OpInput::UseAddress(address_from_bytes(slice[0], slice[1]).wrapping_add(x.into())),
+                    AddressingMode::ABY => OpInput::UseAddress(address_from_bytes(slice[0], slice[1]).wrapping_add(y.into())),
+                    AddressingMode::IND => {
+                        let slice = read_address(memory, address_from_bytes(slice[0], slice[1]));
+                        OpInput::UseAddress(address_from_bytes(slice[0], slice[1]))
+                    },
+                    AddressingMode::IZX => {
+                        let start = slice[0].wrapping_add(x);
+                        let slice = read_address(memory, u16::from(start));
+                        OpInput::UseAddress(address_from_bytes(slice[0], slice[1]))
+                    }
+                    AddressingMode::IZY => {
+                        let start = slice[0];
+                        let slice = read_address(memory, u16::from(start));
+                        OpInput::UseAddress(address_from_bytes(slice[0], slice[1]).wrapping_add(y.into()))
+                    },
+                };
+
+                self.registers.pc = self.registers.pc.wrapping_add(num_bytes);
+
+                Some((instr, am_out))
+            }
+            _ => None,
+        }
+    }
+
+    pub fn execute_instruction(&mut self, decoded_instr: DecodedInstr) {
+        match decoded_instr {
+            (Instruction::BRK, OpInput::UseImmediate(val)) => {
+
+            },
+            _ => panic!()
         }
     }
 }
--- src/instruction.rs
+++ src/instruction.rs
@@ -1,7 +1,16 @@
 pub enum Instruction {
-
+    BRK,
+    ORA,
 }
 
+pub enum OpInput {
+    UseImplied,
+    UseImmediate(u8),
+    UseRelative(u16),
+    UseAddress(u16),
+}
+
+#[derive(Copy, Clone)]
 pub enum AddressingMode {
     IMP,
     IMM,
@@ -17,15 +26,40 @@ pub enum AddressingMode {
     IZY,
 }
 
+impl AddressingMode {
+    pub const fn extra_bytes(self) -> u16 {
+        match self {
+            AddressingMode::IMP => 0,
+            AddressingMode::IMM => 1,
+            AddressingMode::ZP0 => 1,
+            AddressingMode::ZPX => 1,
+            AddressingMode::ZPY => 1,
+            AddressingMode::REL => 1,
+            AddressingMode::ABS => 2,
+            AddressingMode::ABX => 2,
+            AddressingMode::ABY => 2,
+            AddressingMode::IND => 2,
+            AddressingMode::IZX => 1,
+            AddressingMode::IZY => 1,
+        }
+    }
+}
+
+pub type DecodedInstr = (Instruction, OpInput);
+
 pub struct Nmos6502;
 
 impl crate::Variant for Nmos6502 {
-    fn decode(opcode: u8) -> Option<(
-            Instruction,
-            AddressingMode,
-        )> {
-            match opcode {
-                _ => None,
-            } 
+    fn decode(opcode: u8) -> Option<(Instruction, AddressingMode)> {
+        match opcode {
+            0x00 => Some((Instruction::BRK, AddressingMode::IMP)),
+            0x01 => Some((Instruction::ORA, AddressingMode::IZX)),
+            0x02 => None,
+            0x03 => None,
+            0x04 => None,
+            0x05 => Some((Instruction::ORA, AddressingMode::ZP0)),
+            0xFF => None,
+            _ => None,
+        }
     }
 }
